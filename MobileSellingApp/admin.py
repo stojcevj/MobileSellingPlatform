bb18d8f7af319fc29eb993b01c449a68f274d1c3
Jovan <stojcevjovan@hotmail.com>
2023-06-22 14:41:40 +0200

    finished the templates

--- MobileSellingApp/admin.py
+++ MobileSellingApp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from MobileSellingApp.models import MobileListing, Profile
+
+
+class MobileListingAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.listing_profile = request.user
+        obj.save()
+
+
+admin.site.register(MobileListing, MobileListingAdmin)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.profile_user = request.user
+        obj.save()
+
+
+admin.site.register(Profile, ProfileAdmin)
